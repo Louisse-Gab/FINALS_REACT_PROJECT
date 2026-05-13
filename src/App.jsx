c5c39a73b95d171a52bd86e18013120fd2304374
AC <angelcarmrose.apilado.cics@ust.edu.ph>
2026-05-13 12:17:41 +0800

    Describe changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./LandingPage";
+import GetStarted from "./GetStarted";
 import Features from "./Features";
+import AboutUs from "./AboutUs";
 import SignUp from "./SignUp";
 import Login from "./Login";
 import Balance from "./Balance";
 import Currency from "./Currency";
-<<<<<<< HEAD
-import GetStarted from "./GetStarted";
-import AboutUs from "./AboutUs";
-=======
 import Success from "./Success";
 import Dashboard from "./Dashboard";
 import Transaction from "./Transaction";
->>>>>>> cb7dc47e732233c9eb868229aa5c9781eee11031
 
 function App() {
   return (
@@ -22,16 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/features" element={<Features />} />
-<<<<<<< HEAD
-        <Route path="/about" element={<AboutUs />} />
-
-        {/* SIGN UP PAGE */}
-=======
->>>>>>> cb7dc47e732233c9eb868229aa5c9781eee11031
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
 
         <Route path="/getstarted" element={<GetStarted />} /> 
+        <Route path="/about" element={<AboutUs />} /> 
+
         <Route path="/currency" element={<Currency />} />
         <Route path="/balance" element={<Balance />} />
         <Route path="/success" element={<Success />} />
